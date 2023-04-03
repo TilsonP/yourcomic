7da742b1b5b68593256d1be00be80ae4fc80eff2
Tilson Perez <tilsonjr15@gmail.com>
2023-04-02 21:52:30 -0500

    -Se agrega animacion de carga -Se limpia codigo SCSS

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import {Home} from "./screens/home";
 import {useDispatch} from "react-redux";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {comicServices} from "./services/comicService";
+import { PacmanLoader } from 'react-spinners'
 
 function App() {
 
     const dispatch = useDispatch();
     const service = new comicServices()
+
+    const [loading, setLoading] = useState(true)
+    const [comicLoaded, setComicLoaded] = useState(false)
     function loadComic() {
-        service.getRandomComic(dispatch)
+        let load = service.getRandomComic(dispatch)
+        load === "ok" && setComicLoaded(true)
     }
 
     useEffect(() => {
+        setLoading(true)
         loadComic()
+        comicLoaded && setTimeout(() => setLoading(false),2000)
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     return (
         <div className="App">
-            <Home/>
+            {!loading ?
+                <Home/>:
+                <div className="loading">
+                    <PacmanLoader color="#fff" />
+                </div>
+            }
         </div>
     );
 }
